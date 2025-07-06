4b4b15544989552a3fa48caf92666119995e64f2
Olivier Rukabo <olrukabo@asystresources.com>
2025-07-06 11:56:16 +0200

    first commit vue

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>{{ greeting }}</h1>
+  <p>Comment allez vous</p>
+</template>
+
+<script setup>
+const name = 'APDLC ASBL'
+const greeting = `Bienvenue à ${name}!`
+</script>
+
+<style>
+h1{
+  color: orangered;
+}
+</style>
