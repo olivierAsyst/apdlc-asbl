b889f7df6b22818850d846688be86243e6633ab7
Olivier Rukabo <olrukabo@asystresources.com>
2025-07-31 16:55:13 +0200

    les composants

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,93 @@
 <template>
+  <Button>
+    Envoyer
+  </Button>
+  <form action="" @submit.prevent="addTodo">
+    <fieldset role="group">
+      <input 
+        type="text"
+        placeholder="Tâche a faire"
+        v-model="newTodo">
+      <button :disabled="newTodo.length === 0">Ajouter</button>
+    </fieldset>
+  </form>
+  <div v-if="todos.length === 0">
+    Vous n'avez pas des taches a faire :(
+  </div>
+  <div v-else>
+    <ul>
+      <li 
+        v-for="todo in sortedTodos" 
+        key="todo.date"
+        :class="{completed: todo.completed}"
+        >
+        <!--<label>
+          <input type="checkbox" v-model="todo.completed" id="">
+          {{ todo.title }}
+        </label>-->
+        <Checkbox :label="todo.title" v-model="todo.completed"/>
+      </li>
+    </ul>
+    <label>
+      <!--<input type="checkbox" v-model="hideCompleted">-->
+      <Checkbox label="Masquer les taches terminees" v-model="hideCompleted"/>
+    </label>
+    <p v-if="remainingTodos > 0"> <span style="color: orangered; font-weight: bold">{{ remainingTodos }}</span> tache{{ remainingTodos > 1 ? 's' : '' }} a faire</p>
+    <Checkbox label="Cocher"/>
+  </div>
+</template>
+
+<script setup>
+import { computed, ref } from 'vue';
+import Checkbox from './Checkbox.vue';
+import Button from './Button.vue';
+
+const newTodo = ref('')
+const hideCompleted = ref(false)
+const todos = ref([
+  {
+    title: 'Tache de test',
+    completed: true,
+    date: 1
+  },
+  {
+    title: 'Tache a faire',
+    completed: false,
+    date: 2
+  }
+]);
+const addTodo = () =>{
+  todos.value.push({
+    title: newTodo.value,
+    completed: false,
+    date: Date.now()
+  })
+  newTodo.value = ''
+}
+const sortedTodos = computed( () =>{
+  const sortedTodos = todos.value.toSorted((a, b) => a.completed > b.completed ? 1 : -1)
+  if (hideCompleted.value === true){
+    return sortedTodos.filter(t => t.completed === false)
+  }
+  return sortedTodos;
+})
+
+const remainingTodos = computed (() =>{
+  console.log("remaining");
+  
+  return todos.value.filter(t => t.completed === false).length
+})
+
+</script>
+<style>
+.completed{
+  opacity: .5;
+  text-decoration: line-through;
+}
+</style>
+
+<!--COURS 1
+<template>
   <h1>{{ greeting }}</h1>
   <div v-html="list"></div>
   <p :style="{color: count < 5 ? 'red' : 'green'}">Le compteur</p>
@@ -7,7 +96,7 @@
   <div v-if="count==0">Incrementer pour commencer</div>
   <div v-else>
     <div v-if="count >= 5">Vous avez de depassee 10 fois</div>
-    <div v-else>Vous avez de moins 10 fois</div>
+    <div v-else>Vous avez de moins 5 fois</div>
   </div>
   <div class="btns">
     <button v-on:click="increment">Incrémenter</button>
@@ -20,7 +109,7 @@
   </class>
   <ul>
     <li v-for="book in livresesoteriques">
-      {{ book }} <button @click="deleteBook(book)">Supprime</button>
+      {{ book }} <button @click="deleteBook(book)">Supprimer</button>
     </li>
   </ul>
 </template>
@@ -92,4 +181,4 @@ button{
 button:hover{
   background-color: darkorange;
 }
-</style>
+</style> -->
---
+++ src/Checkbox.vue
@@ -0,0 +1,13 @@
+<template>
+<label>
+    <input type="checkbox" v-model="model">
+    {{ label }}
+</label>
+</template>
+<script setup>
+const model = defineModel();
+defineProps({
+    label: String
+})
+defineEmits(['check', 'uncheck'])
+</script>
